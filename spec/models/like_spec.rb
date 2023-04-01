71668800732bb22da89f5c5671bbffed95f40dd2
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-02 00:02:11 +0430

    Add unit specs for likes_counter in like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -16,4 +16,28 @@ RSpec.describe Like, type: :model do
       expect(id).to be == 3
     end
   end
+it 'increments likes_counter' do
+    user = User.new(
+      name: 'John Doe',
+      bio: 'Developer',
+    )
+    user.save
+
+    post = Post.new(
+      title: 'My first post',
+      text: 'Hello world!',
+      comments_counter: 4,
+      likes_counters: 5,
+      author_id: user.id
+    )
+    post.save
+
+    like = Like.new(
+      author_id: user.id,
+      post_id: post.id
+    )
+    like.save
+
+    expect(post.likes_counters).to_not eq(6)
+  end
 end
