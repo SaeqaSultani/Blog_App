bce964c9270b72c5ef3491af441af846c52043c9
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-07 15:07:29 +0430

    Added controllers specs to the project

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'users#index'
+
   resources :users, only: [:show,:index] do    
     resources :posts, only: [:index, :show]     
        end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,15 +3,19 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+      # get '/users/index'
+      get root_path
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include('Here is a list of users')
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/show'
-      expect(response).to have_http_status(:success)
+      # get '/users/show'
+      get user_path(id: 1)
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include('Here is a spacific user')
     end
   end
 end
