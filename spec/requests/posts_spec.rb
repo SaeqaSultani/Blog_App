0a555fa4baadd6435ed415e9f1485ae2ad95c7c3
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-07 17:09:00 +0430

    Added test  for rendering correct view template in the posts_rspec.rb file

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to have_http_status(:ok)
       expect(response.body).to include('Here is a list of posts for a given user')
     end
+
+
+    it 'Should renders index template' do
+
+      get user_posts_path(user_id: 1)
+      expect(response).to render_template('index')
+    end
   end
 
   describe 'GET /show' do
@@ -15,5 +22,11 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to have_http_status(:ok)
       expect(response.body).to include('Here is a spacific post')
     end
+
+    it 'Should renders show template' do
+      get user_post_path(user_id: 1, id: 1)
+      expect(response).to render_template('show')
+    end
+
   end
 end
