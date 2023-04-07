2b75f8d8f493c5b91fdcf08a395aa1cfe0774eb3
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-07 16:58:20 +0430

    Added test  for rendering correct view template in the user_rspec.rb file

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,19 +3,29 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      # get '/users/index'
-      get root_path
+      get users_path
       expect(response).to have_http_status(:ok)
       expect(response.body).to include('Here is a list of users')
     end
+
+    it 'renders the index template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      # get '/users/show'
       get user_path(id: 1)
       expect(response).to have_http_status(:ok)
       expect(response.body).to include('Here is a spacific user')
     end
+
+    it 'renders the show template' do
+      get user_path(id: 1)
+      expect(response).to render_template(:show)
+    end
+
   end
 end
