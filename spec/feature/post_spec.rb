3f04f178fb0053f3b4893fdb908b938f4f5e6d91
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-14 22:42:59 +0430

    Added comments and new commender for username of each commentor and see the comment each commentor left in  post_spec file

--- spec/feature/post_spec.rb
+++ spec/feature/post_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :system do
   user = User.create(name: 'John', posts_counter: 30, photo: 'https://randomuser.me/api/portraits/men/70.jpg',
                      bio: 'Teacher from Poland.')
+  user1 = User.create(name: 'Ali', posts_counter: 20, photo: 'https://randomuser.me/api/portraits/men/80.jpg',
+                      bio: 'Teacher from Landan.')                   
 
   subject do
     Post.new(author_id: user.id, title: 'First Post', text: 'My first post', comments_counter: 20, likes_counters: 30)
@@ -91,14 +93,21 @@ RSpec.describe Post, type: :system do
     end
 
     it 'I can see the username of each commentor.' do
-      comment = Comment.create(author_id: user.id, post_id: subject.id, text: 'Nice post')
+      comment1 = Comment.create(author_id: user.id, post_id: subject.id, text: 'Nice post')
+      comment2 = Comment.create(author_id: user1.id, post_id: subject.id, text: 'I like this post')
+
       visit user_post_path(user.id, subject.id)
-      page.has_content?(comment.author.name)
+      visit user_post_path(user1.id, subject.id)
+      page.has_content?(comment1.author.name)
+      page.has_content?(comment2.author.name)
     end
     it 'I can see the comment each commentor left.' do
-      comment = Comment.create(author_id: user.id, post_id: subject.id, text: 'Nice post')
+      comment1 = Comment.create(author_id: user.id, post_id: subject.id, text: 'Nice post')
+      comment2 = Comment.create(author_id: user1.id, post_id: subject.id, text: 'I like this post')
       visit user_post_path(user.id, subject.id)
-      page.has_content?(comment.text)
+      visit user_post_path(user1.id, subject.id)
+      page.has_content?(comment1.text)
+      page.has_content?(comment2.text)
     end
   end
 end
