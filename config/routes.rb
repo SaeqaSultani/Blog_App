fb5ee3273545d91defa54392e8501b93f56f9265
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-16 03:30:30 +0430

    Fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,6 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  # get 'comments/new'
-  # get 'comments/create'
-  # get 'likes/new'
-  # get 'likes/create'
-  # get 'like/new'
-  # get 'like/create'
-  # get 'comment/new'
-  # get 'comment/create'
-  # get 'posts/index'
-  # get 'posts/show'
-  # get 'users/index'
-  # get 'users/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
